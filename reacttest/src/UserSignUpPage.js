42599ee3de954f8c530eba119f7d035533598c6a
uguraltintass <altintasugur7@gmail.com>
2023-02-12 16:49:33 +0300

    first commit

---
+++ reacttest/src/UserSignUpPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserSignUpPage extends React.Component{
+
+    render() {
+        return (
+            <h1>Sign Up</h1>
+        );
+    }
+
+}
+
+export default UserSignUpPage;
